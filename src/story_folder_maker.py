f81ef39303ca0251c938dd57fa3f638c03be96bb
nborland <nborland@kalkomey.com>
2026-04-16 16:08:11 -0400

    Add a script to create a folder & character files, add The_Office character tropes

---
+++ src/story_folder_maker.py
@@ -0,0 +1,13 @@
+import sys
+from pathlib import Path
+
+show_name = sys.argv[1]
+character_names = sys.argv[2]
+
+folder_path = Path(f'TV_Tropes_data/{show_name.replace(' ', '_')}')
+file_names = character_names.replace(' ', '_').split('\n')
+
+folder_path.mkdir()
+for name in file_names:
+    file_path = folder_path / (name + '.txt')
+    file_path.touch()
